df3e34366ebb407c4559aa083386b788585ce7b1
Jenkins <jenkins@kaltura.com>
2019-02-17 09:49:51 +0000

    Auto-generated by Jenkins

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaSingleSegmentValue.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaSingleSegmentValue.ts
@@ -10,7 +10,6 @@ export interface KalturaSingleSegmentValueArgs  extends KalturaBaseSegmentValueA
 export class KalturaSingleSegmentValue extends KalturaBaseSegmentValue {
 
     readonly id : number;
-	readonly affectedUsers : number;
 
     constructor(data? : KalturaSingleSegmentValueArgs)
     {
@@ -24,8 +23,7 @@ export class KalturaSingleSegmentValue extends KalturaBaseSegmentValue {
             result.properties,
             {
                 objectType : { type : 'c', default : 'KalturaSingleSegmentValue' },
-				id : { type : 'n', readOnly : true },
-				affectedUsers : { type : 'n', readOnly : true }
+				id : { type : 'n', readOnly : true }
             }
         );
         return result;
