7d0b8644d2cf8327d03b896f1976d38729997bbb
Backend CI <ott.rnd.core@kaltura.com>
2019-03-21 08:41:20 +0000

    Auto-generated by Jenkins

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaIngestProfileListResponse.ts
@@ -0,0 +1,35 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaIngestProfile } from './KalturaIngestProfile';
+import { KalturaListResponse, KalturaListResponseArgs } from './KalturaListResponse';
+
+export interface KalturaIngestProfileListResponseArgs  extends KalturaListResponseArgs {
+    objects? : KalturaIngestProfile[];
+}
+
+
+export class KalturaIngestProfileListResponse extends KalturaListResponse {
+
+    objects : KalturaIngestProfile[];
+
+    constructor(data? : KalturaIngestProfileListResponseArgs)
+    {
+        super(data);
+        if (typeof this.objects === 'undefined') this.objects = [];
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaIngestProfileListResponse' },
+				objects : { type : 'a', subTypeConstructor : KalturaIngestProfile, subType : 'KalturaIngestProfile' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaIngestProfileListResponse'] = KalturaIngestProfileListResponse;
