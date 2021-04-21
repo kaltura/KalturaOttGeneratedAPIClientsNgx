1c4425047e6808741a28e63fc235c464b7167f5b
Backend CI <ott.rnd.core@kaltura.com>
2021-04-21 20:10:10 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/447, branch 6_3_1

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaBaseSearchAssetFilter.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaBaseSearchAssetFilter.ts
@@ -2,12 +2,14 @@
 import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
 import { KalturaAssetGroupBy } from './KalturaAssetGroupBy';
 import { KalturaGroupByOrder } from './KalturaGroupByOrder';
+import { KalturaGroupingOption } from './KalturaGroupingOption';
 import { KalturaAssetFilter, KalturaAssetFilterArgs } from './KalturaAssetFilter';
 
 export interface KalturaBaseSearchAssetFilterArgs  extends KalturaAssetFilterArgs {
     kSql? : string;
 	groupBy? : KalturaAssetGroupBy[];
 	groupOrderBy? : KalturaGroupByOrder;
+	groupingOptionEqual? : KalturaGroupingOption;
 }
 
 
@@ -16,6 +18,7 @@ export class KalturaBaseSearchAssetFilter extends KalturaAssetFilter {
     kSql : string;
 	groupBy : KalturaAssetGroupBy[];
 	groupOrderBy : KalturaGroupByOrder;
+	groupingOptionEqual : KalturaGroupingOption;
 
     constructor(data? : KalturaBaseSearchAssetFilterArgs)
     {
@@ -32,7 +35,8 @@ export class KalturaBaseSearchAssetFilter extends KalturaAssetFilter {
                 objectType : { type : 'c', default : 'KalturaBaseSearchAssetFilter' },
 				kSql : { type : 's' },
 				groupBy : { type : 'a', subTypeConstructor : KalturaAssetGroupBy, subType : 'KalturaAssetGroupBy' },
-				groupOrderBy : { type : 'es', subTypeConstructor : KalturaGroupByOrder, subType : 'KalturaGroupByOrder' }
+				groupOrderBy : { type : 'es', subTypeConstructor : KalturaGroupByOrder, subType : 'KalturaGroupByOrder' },
+				groupingOptionEqual : { type : 'es', subTypeConstructor : KalturaGroupingOption, subType : 'KalturaGroupingOption' }
             }
         );
         return result;
