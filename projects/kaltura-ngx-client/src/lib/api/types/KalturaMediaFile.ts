b11874f576dd8ddfc736be59d2bee14a070f1578
Backend CI <ott.rnd.core@kaltura.com>
2021-05-04 09:10:23 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/450, branch 6_2_3

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaMediaFile.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaMediaFile.ts
@@ -6,7 +6,6 @@ import { KalturaAssetFile, KalturaAssetFileArgs } from './KalturaAssetFile';
 export interface KalturaMediaFileArgs  extends KalturaAssetFileArgs {
     assetId? : number;
 	typeId? : number;
-	altUrl? : string;
 	duration? : number;
 	externalId? : string;
 	altExternalId? : string;
@@ -35,7 +34,6 @@ export class KalturaMediaFile extends KalturaAssetFile {
 	readonly id : number;
 	readonly type : string;
 	typeId : number;
-	altUrl : string;
 	duration : number;
 	externalId : string;
 	altExternalId : string;
@@ -72,7 +70,6 @@ export class KalturaMediaFile extends KalturaAssetFile {
 				id : { type : 'n', readOnly : true },
 				type : { type : 's', readOnly : true },
 				typeId : { type : 'n' },
-				altUrl : { type : 's' },
 				duration : { type : 'n' },
 				externalId : { type : 's' },
 				altExternalId : { type : 's' },
