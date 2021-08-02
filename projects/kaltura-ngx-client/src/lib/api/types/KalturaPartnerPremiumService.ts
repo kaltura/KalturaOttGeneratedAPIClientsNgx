5b57421016fab2fdc5deb38f25fff8d1abd5d4c3
Backend CI <ott.rnd.core@kaltura.com>
2021-08-02 08:52:10 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/564, branch 671_BEO-10410_PremiumService_mng

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaPartnerPremiumService.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaPartnerPremiumService.ts
@@ -2,18 +2,20 @@
 import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
 import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
 
-export interface KalturaPremiumServiceArgs  extends KalturaObjectBaseArgs {
+export interface KalturaPartnerPremiumServiceArgs  extends KalturaObjectBaseArgs {
     id? : number;
 	name? : string;
+	isApplied? : boolean;
 }
 
 
-export class KalturaPremiumService extends KalturaObjectBase {
+export class KalturaPartnerPremiumService extends KalturaObjectBase {
 
     id : number;
 	name : string;
+	isApplied : boolean;
 
-    constructor(data? : KalturaPremiumServiceArgs)
+    constructor(data? : KalturaPartnerPremiumServiceArgs)
     {
         super(data);
     }
@@ -24,13 +26,14 @@ export class KalturaPremiumService extends KalturaObjectBase {
         Object.assign(
             result.properties,
             {
-                objectType : { type : 'c', default : 'KalturaPremiumService' },
+                objectType : { type : 'c', default : 'KalturaPartnerPremiumService' },
 				id : { type : 'n' },
-				name : { type : 's' }
+				name : { type : 's' },
+				isApplied : { type : 'b' }
             }
         );
         return result;
     }
 }
 
-typesMappingStorage['KalturaPremiumService'] = KalturaPremiumService;
+typesMappingStorage['KalturaPartnerPremiumService'] = KalturaPartnerPremiumService;
