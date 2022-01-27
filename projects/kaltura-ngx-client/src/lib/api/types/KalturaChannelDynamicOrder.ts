c3045d070d304aa6b7e17e944eaffd42ebd0c0ed
Backend CI <ott.rnd.core@kaltura.com>
2022-01-27 00:16:04 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/759, branch 7_2_0_1

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaChannelDynamicOrder.ts
@@ -0,0 +1,37 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaMetaTagOrderBy } from './KalturaMetaTagOrderBy';
+import { KalturaBaseChannelOrder, KalturaBaseChannelOrderArgs } from './KalturaBaseChannelOrder';
+
+export interface KalturaChannelDynamicOrderArgs  extends KalturaBaseChannelOrderArgs {
+    name? : string;
+	orderBy? : KalturaMetaTagOrderBy;
+}
+
+
+export class KalturaChannelDynamicOrder extends KalturaBaseChannelOrder {
+
+    name : string;
+	orderBy : KalturaMetaTagOrderBy;
+
+    constructor(data? : KalturaChannelDynamicOrderArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaChannelDynamicOrder' },
+				name : { type : 's' },
+				orderBy : { type : 'es', subTypeConstructor : KalturaMetaTagOrderBy, subType : 'KalturaMetaTagOrderBy' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaChannelDynamicOrder'] = KalturaChannelDynamicOrder;
---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaIngestEpg.ts
@@ -0,0 +1,54 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaIngestStatus } from './KalturaIngestStatus';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaIngestEpgArgs  extends KalturaObjectBaseArgs {
+    ingestName? : string;
+	ingestFilenameExtension? : string;
+	createdDate? : number;
+	ingestedByUserId? : number;
+	completedDate? : number;
+	ingestProfileId? : number;
+	status? : KalturaIngestStatus;
+}
+
+
+export class KalturaIngestEpg extends KalturaObjectBase {
+
+    readonly ingestId : number;
+	ingestName : string;
+	ingestFilenameExtension : string;
+	createdDate : number;
+	ingestedByUserId : number;
+	completedDate : number;
+	ingestProfileId : number;
+	status : KalturaIngestStatus;
+
+    constructor(data? : KalturaIngestEpgArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaIngestEpg' },
+				ingestId : { type : 'n', readOnly : true },
+				ingestName : { type : 's' },
+				ingestFilenameExtension : { type : 's' },
+				createdDate : { type : 'n' },
+				ingestedByUserId : { type : 'n' },
+				completedDate : { type : 'n' },
+				ingestProfileId : { type : 'n' },
+				status : { type : 'es', subTypeConstructor : KalturaIngestStatus, subType : 'KalturaIngestStatus' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaIngestEpg'] = KalturaIngestEpg;
