727dfc7a69ee1ac1b16b9c6b0348500d70b1fad4
Backend CI <ott.rnd.core@kaltura.com>
2022-04-05 07:57:35 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/849, branch 750_BEO-11760_shop-condition-filtering

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaDeviceFamilyFilter.ts
@@ -0,0 +1,40 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaDeviceFamilyType } from './KalturaDeviceFamilyType';
+import { KalturaFilter, KalturaFilterArgs } from './KalturaFilter';
+
+export interface KalturaDeviceFamilyFilterArgs  extends KalturaFilterArgs {
+    idEqual? : number;
+	nameEqual? : string;
+	typeEqual? : KalturaDeviceFamilyType;
+}
+
+
+export class KalturaDeviceFamilyFilter extends KalturaFilter {
+
+    idEqual : number;
+	nameEqual : string;
+	typeEqual : KalturaDeviceFamilyType;
+
+    constructor(data? : KalturaDeviceFamilyFilterArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaDeviceFamilyFilter' },
+				idEqual : { type : 'n' },
+				nameEqual : { type : 's' },
+				typeEqual : { type : 'es', subTypeConstructor : KalturaDeviceFamilyType, subType : 'KalturaDeviceFamilyType' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaDeviceFamilyFilter'] = KalturaDeviceFamilyFilter;
