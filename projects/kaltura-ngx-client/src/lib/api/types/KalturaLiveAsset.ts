0cdca37f4aaaf818ee928172faac3bf0ac7a36ef
Backend CI <ott.rnd.core@kaltura.com>
2022-05-19 12:22:57 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/899, branch BEO-11720_linear_channels_updated

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaLiveAsset.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaLiveAsset.ts
@@ -9,8 +9,8 @@ export interface KalturaLiveAssetArgs  extends KalturaMediaAssetArgs {
 	enableCatchUpState? : KalturaTimeShiftedTvState;
 	enableStartOverState? : KalturaTimeShiftedTvState;
 	bufferCatchUpSetting? : number;
-	paddingBeforeProgramStartsSetting? : number;
-	paddingAfterProgramEndsSetting? : number;
+	paddingBeforeProgramStarts? : number;
+	paddingAfterProgramEnds? : number;
 	bufferTrickPlaySetting? : number;
 	enableRecordingPlaybackNonEntitledChannelState? : KalturaTimeShiftedTvState;
 	enableTrickPlayState? : KalturaTimeShiftedTvState;
@@ -26,8 +26,8 @@ export class KalturaLiveAsset extends KalturaMediaAsset {
 	enableCatchUpState : KalturaTimeShiftedTvState;
 	enableStartOverState : KalturaTimeShiftedTvState;
 	bufferCatchUpSetting : number;
-	paddingBeforeProgramStartsSetting : number;
-	paddingAfterProgramEndsSetting : number;
+	paddingBeforeProgramStarts : number;
+	paddingAfterProgramEnds : number;
 	bufferTrickPlaySetting : number;
 	enableRecordingPlaybackNonEntitledChannelState : KalturaTimeShiftedTvState;
 	enableTrickPlayState : KalturaTimeShiftedTvState;
@@ -37,8 +37,6 @@ export class KalturaLiveAsset extends KalturaMediaAsset {
 	readonly enableCatchUp : boolean;
 	readonly enableStartOver : boolean;
 	readonly catchUpBuffer : number;
-	readonly paddingBeforeProgramStarts : number;
-	readonly paddingAfterProgramEnds : number;
 	readonly trickPlayBuffer : number;
 	readonly enableRecordingPlaybackNonEntitledChannel : boolean;
 	readonly enableTrickPlay : boolean;
@@ -60,8 +58,8 @@ export class KalturaLiveAsset extends KalturaMediaAsset {
 				enableCatchUpState : { type : 'es', subTypeConstructor : KalturaTimeShiftedTvState, subType : 'KalturaTimeShiftedTvState' },
 				enableStartOverState : { type : 'es', subTypeConstructor : KalturaTimeShiftedTvState, subType : 'KalturaTimeShiftedTvState' },
 				bufferCatchUpSetting : { type : 'n' },
-				paddingBeforeProgramStartsSetting : { type : 'n' },
-				paddingAfterProgramEndsSetting : { type : 'n' },
+				paddingBeforeProgramStarts : { type : 'n' },
+				paddingAfterProgramEnds : { type : 'n' },
 				bufferTrickPlaySetting : { type : 'n' },
 				enableRecordingPlaybackNonEntitledChannelState : { type : 'es', subTypeConstructor : KalturaTimeShiftedTvState, subType : 'KalturaTimeShiftedTvState' },
 				enableTrickPlayState : { type : 'es', subTypeConstructor : KalturaTimeShiftedTvState, subType : 'KalturaTimeShiftedTvState' },
@@ -71,8 +69,6 @@ export class KalturaLiveAsset extends KalturaMediaAsset {
 				enableCatchUp : { type : 'b', readOnly : true },
 				enableStartOver : { type : 'b', readOnly : true },
 				catchUpBuffer : { type : 'n', readOnly : true },
-				paddingBeforeProgramStarts : { type : 'n', readOnly : true },
-				paddingAfterProgramEnds : { type : 'n', readOnly : true },
 				trickPlayBuffer : { type : 'n', readOnly : true },
 				enableRecordingPlaybackNonEntitledChannel : { type : 'b', readOnly : true },
 				enableTrickPlay : { type : 'b', readOnly : true },
