84e4d6338f6b160f54d96ba5dabe2eaedc59e091
Backend CI <ott.rnd.core@kaltura.com>
2023-01-19 07:56:47 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1035, branch 8_4_1_1

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaPaddedRecording.ts
@@ -0,0 +1,36 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaRecording, KalturaRecordingArgs } from './KalturaRecording';
+
+export interface KalturaPaddedRecordingArgs  extends KalturaRecordingArgs {
+    startPadding? : number;
+	endPadding? : number;
+}
+
+
+export class KalturaPaddedRecording extends KalturaRecording {
+
+    startPadding : number;
+	endPadding : number;
+
+    constructor(data? : KalturaPaddedRecordingArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaPaddedRecording' },
+				startPadding : { type : 'n' },
+				endPadding : { type : 'n' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaPaddedRecording'] = KalturaPaddedRecording;
