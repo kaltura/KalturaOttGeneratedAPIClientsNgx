14007c27d884e3aacd6a48f635f15ba04bafd2de
Backend CI <ott.rnd.core@kaltura.com>
2023-07-13 11:00:41 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1170, branch 8_9_1-java

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaVodIngestAssetResultList.ts
@@ -0,0 +1,35 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaVodIngestAssetResult } from './KalturaVodIngestAssetResult';
+import { KalturaListResponse, KalturaListResponseArgs } from './KalturaListResponse';
+
+export interface KalturaVodIngestAssetResultListArgs  extends KalturaListResponseArgs {
+    objects? : KalturaVodIngestAssetResult[];
+}
+
+
+export class KalturaVodIngestAssetResultList extends KalturaListResponse {
+
+    objects : KalturaVodIngestAssetResult[];
+
+    constructor(data? : KalturaVodIngestAssetResultListArgs)
+    {
+        super(data);
+        if (typeof this.objects === 'undefined') this.objects = [];
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaVodIngestAssetResultList' },
+				objects : { type : 'a', subTypeConstructor : KalturaVodIngestAssetResult, subType : 'KalturaVodIngestAssetResult' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaVodIngestAssetResultList'] = KalturaVodIngestAssetResultList;
