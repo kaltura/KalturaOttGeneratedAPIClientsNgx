a7f8dfb794b0a9fb9d2b67ab44e7b189657ebec5
Backend CI <ott.rnd.core@kaltura.com>
2023-08-02 12:32:56 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1190, branch 1000_BEO-14331_region-external-id-expose

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaLineupRegionalChannelFilter.ts
@@ -0,0 +1,45 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaFilter, KalturaFilterArgs } from './KalturaFilter';
+
+export interface KalturaLineupRegionalChannelFilterArgs  extends KalturaFilterArgs {
+    regionIdEqual? : number;
+	parentRegionIncluded? : boolean;
+	kSql? : string;
+	lcnGreaterThanOrEqual? : number;
+	lcnLessThanOrEqual? : number;
+}
+
+
+export class KalturaLineupRegionalChannelFilter extends KalturaFilter {
+
+    regionIdEqual : number;
+	parentRegionIncluded : boolean;
+	kSql : string;
+	lcnGreaterThanOrEqual : number;
+	lcnLessThanOrEqual : number;
+
+    constructor(data? : KalturaLineupRegionalChannelFilterArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaLineupRegionalChannelFilter' },
+				regionIdEqual : { type : 'n' },
+				parentRegionIncluded : { type : 'b' },
+				kSql : { type : 's' },
+				lcnGreaterThanOrEqual : { type : 'n' },
+				lcnLessThanOrEqual : { type : 'n' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaLineupRegionalChannelFilter'] = KalturaLineupRegionalChannelFilter;
