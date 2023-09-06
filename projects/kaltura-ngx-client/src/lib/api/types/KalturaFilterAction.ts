1a3fea649038e0dc6db7421f8be323a6e9e07f04
Backend CI <ott.rnd.core@kaltura.com>
2023-09-06 07:02:10 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1203, branch BEO-14481-1

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaFilterAction.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaFilterAction.ts
@@ -1,15 +1,16 @@
 
 import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaBasePreActionCondition } from './KalturaBasePreActionCondition';
 import { KalturaAssetRuleAction, KalturaAssetRuleActionArgs } from './KalturaAssetRuleAction';
 
 export interface KalturaFilterActionArgs  extends KalturaAssetRuleActionArgs {
-    
+    preActionCondition? : KalturaBasePreActionCondition;
 }
 
 
 export class KalturaFilterAction extends KalturaAssetRuleAction {
 
-    
+    preActionCondition : KalturaBasePreActionCondition;
 
     constructor(data? : KalturaFilterActionArgs)
     {
@@ -22,7 +23,8 @@ export class KalturaFilterAction extends KalturaAssetRuleAction {
         Object.assign(
             result.properties,
             {
-                objectType : { type : 'c', default : 'KalturaFilterAction' }
+                objectType : { type : 'c', default : 'KalturaFilterAction' },
+				preActionCondition : { type : 'o', subTypeConstructor : KalturaBasePreActionCondition, subType : 'KalturaBasePreActionCondition' }
             }
         );
         return result;
