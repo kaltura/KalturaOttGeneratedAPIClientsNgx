f6fd3ee2e2d74db70c738460b6babaf938c9a979
Backend CI <ott.rnd.core@kaltura.com>
2023-11-16 14:01:16 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1243, branch BEO-14631_Coupon_Management_Generate_Coupons

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaCouponFilesLinks.ts
@@ -0,0 +1,35 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaStringValue } from './KalturaStringValue';
+import { KalturaListResponse, KalturaListResponseArgs } from './KalturaListResponse';
+
+export interface KalturaCouponFilesLinksArgs  extends KalturaListResponseArgs {
+    objects? : KalturaStringValue[];
+}
+
+
+export class KalturaCouponFilesLinks extends KalturaListResponse {
+
+    objects : KalturaStringValue[];
+
+    constructor(data? : KalturaCouponFilesLinksArgs)
+    {
+        super(data);
+        if (typeof this.objects === 'undefined') this.objects = [];
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaCouponFilesLinks' },
+				objects : { type : 'a', subTypeConstructor : KalturaStringValue, subType : 'KalturaStringValue' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaCouponFilesLinks'] = KalturaCouponFilesLinks;
