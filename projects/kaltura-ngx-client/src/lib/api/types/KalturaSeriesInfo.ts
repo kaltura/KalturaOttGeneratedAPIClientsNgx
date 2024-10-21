c473a4703f5a920b20ec104cf001b97ec2a340df
Backend CI <ott.rnd.core@kaltura.com>
2024-10-21 16:46:50 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1354, branch BEO-16198_activate_wbr_permission

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaSeriesInfo.ts
@@ -0,0 +1,39 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaSeriesInfoArgs  extends KalturaObjectBaseArgs {
+    seriesIdMetadataName? : string;
+	seriesTypeId? : number;
+	episodeTypeId? : number;
+}
+
+
+export class KalturaSeriesInfo extends KalturaObjectBase {
+
+    seriesIdMetadataName : string;
+	seriesTypeId : number;
+	episodeTypeId : number;
+
+    constructor(data? : KalturaSeriesInfoArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaSeriesInfo' },
+				seriesIdMetadataName : { type : 's' },
+				seriesTypeId : { type : 'n' },
+				episodeTypeId : { type : 'n' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaSeriesInfo'] = KalturaSeriesInfo;
---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaWatchBasedRecommendationsProfile.ts
@@ -0,0 +1,66 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaSeriesInfo } from './KalturaSeriesInfo';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaWatchBasedRecommendationsProfileArgs  extends KalturaObjectBaseArgs {
+    name? : string;
+	topicIds? : string;
+	analysisMediaTypeIds? : string;
+	userInterestPlayThresholdInPercentages? : number;
+	numberOfInterests? : number;
+	fallbackChannelId? : number;
+	minPlaybacks? : number;
+	maxPlaybacks? : number;
+	allowedRecommendationsKsql? : string;
+	recommendationsMediaTypeIds? : string;
+	recommendationsSeriesInfo? : KalturaSeriesInfo;
+}
+
+
+export class KalturaWatchBasedRecommendationsProfile extends KalturaObjectBase {
+
+    readonly id : number;
+	name : string;
+	topicIds : string;
+	analysisMediaTypeIds : string;
+	userInterestPlayThresholdInPercentages : number;
+	numberOfInterests : number;
+	fallbackChannelId : number;
+	minPlaybacks : number;
+	maxPlaybacks : number;
+	allowedRecommendationsKsql : string;
+	recommendationsMediaTypeIds : string;
+	recommendationsSeriesInfo : KalturaSeriesInfo;
+
+    constructor(data? : KalturaWatchBasedRecommendationsProfileArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaWatchBasedRecommendationsProfile' },
+				id : { type : 'n', readOnly : true },
+				name : { type : 's' },
+				topicIds : { type : 's' },
+				analysisMediaTypeIds : { type : 's' },
+				userInterestPlayThresholdInPercentages : { type : 'n' },
+				numberOfInterests : { type : 'n' },
+				fallbackChannelId : { type : 'n' },
+				minPlaybacks : { type : 'n' },
+				maxPlaybacks : { type : 'n' },
+				allowedRecommendationsKsql : { type : 's' },
+				recommendationsMediaTypeIds : { type : 's' },
+				recommendationsSeriesInfo : { type : 'o', subTypeConstructor : KalturaSeriesInfo, subType : 'KalturaSeriesInfo' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaWatchBasedRecommendationsProfile'] = KalturaWatchBasedRecommendationsProfile;
