f5c9ce45bfa294053c4c5d9cd0d4e56fde752be3
Backend CI <ott.rnd.core@kaltura.com>
2025-01-08 17:45:58 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1403, branch BEO-16562

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaAiMetadataGeneratorConfiguration.ts
@@ -0,0 +1,40 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaStringValue } from './KalturaStringValue';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaAiMetadataGeneratorConfigurationArgs  extends KalturaObjectBaseArgs {
+    isEnabled? : boolean;
+	metaFieldNameMap? : { [key : string] : KalturaStringValue};
+	vectorizedMetaIds? : string;
+}
+
+
+export class KalturaAiMetadataGeneratorConfiguration extends KalturaObjectBase {
+
+    isEnabled : boolean;
+	metaFieldNameMap : { [key : string] : KalturaStringValue};
+	vectorizedMetaIds : string;
+
+    constructor(data? : KalturaAiMetadataGeneratorConfigurationArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaAiMetadataGeneratorConfiguration' },
+				isEnabled : { type : 'b' },
+				metaFieldNameMap : { type : 'm', subTypeConstructor : KalturaStringValue, subType : 'KalturaStringValue' },
+				vectorizedMetaIds : { type : 's' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaAiMetadataGeneratorConfiguration'] = KalturaAiMetadataGeneratorConfiguration;
