3e1803f6776d97241eb12d9fae27e6a272a485df
Backend CI <ott.rnd.core@kaltura.com>
2025-04-04 10:13:02 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1454, branch 11.1.0.1

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaUserLog.ts
@@ -0,0 +1,39 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaUserLogArgs  extends KalturaObjectBaseArgs {
+    
+}
+
+
+export class KalturaUserLog extends KalturaObjectBase {
+
+    readonly id : number;
+	readonly createDate : number;
+	readonly userId : number;
+	readonly message : string;
+
+    constructor(data? : KalturaUserLogArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaUserLog' },
+				id : { type : 'n', readOnly : true },
+				createDate : { type : 'n', readOnly : true },
+				userId : { type : 'n', readOnly : true },
+				message : { type : 's', readOnly : true }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaUserLog'] = KalturaUserLog;
