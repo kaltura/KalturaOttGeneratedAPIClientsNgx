facef245ce7ac64f089601efc1e02c39f0acfbd1
Backend CI <ott.rnd.core@kaltura.com>
2025-04-24 13:24:04 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1460, branch BEO-17082-BEO-17133

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaGenerateSemanticQuery.ts
@@ -0,0 +1,33 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaGenerateSemanticQueryArgs  extends KalturaObjectBaseArgs {
+    query? : string;
+}
+
+
+export class KalturaGenerateSemanticQuery extends KalturaObjectBase {
+
+    query : string;
+
+    constructor(data? : KalturaGenerateSemanticQueryArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaGenerateSemanticQuery' },
+				query : { type : 's' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaGenerateSemanticQuery'] = KalturaGenerateSemanticQuery;
