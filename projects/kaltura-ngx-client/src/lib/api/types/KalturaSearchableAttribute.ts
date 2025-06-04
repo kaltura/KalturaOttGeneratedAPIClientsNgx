46348f166cba67db5122f3d15ea6bc34de7d2728
Backend CI <ott.rnd.core@kaltura.com>
2025-06-04 04:26:33 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1474, branch BEO-17234

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaSearchableAttribute.ts
@@ -0,0 +1,36 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaSearchableAttributeArgs  extends KalturaObjectBaseArgs {
+    assetStructId? : number;
+	attributes? : string;
+}
+
+
+export class KalturaSearchableAttribute extends KalturaObjectBase {
+
+    assetStructId : number;
+	attributes : string;
+
+    constructor(data? : KalturaSearchableAttributeArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaSearchableAttribute' },
+				assetStructId : { type : 'n' },
+				attributes : { type : 's' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaSearchableAttribute'] = KalturaSearchableAttribute;
