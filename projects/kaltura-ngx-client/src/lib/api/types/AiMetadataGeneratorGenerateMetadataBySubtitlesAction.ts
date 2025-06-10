e3dbaedb836d11d39fe37706d0bfb72dc7b7e1c1
Backend CI <ott.rnd.core@kaltura.com>
2025-06-10 07:40:55 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1482, branch BEO-17234-v2

---
+++ projects/kaltura-ngx-client/src/lib/api/types/AiMetadataGeneratorGenerateMetadataBySubtitlesAction.ts
@@ -0,0 +1,47 @@
+
+import { KalturaObjectMetadata } from '../kaltura-object-base';
+import { KalturaGenerateMetadataBySubtitlesJob } from './KalturaGenerateMetadataBySubtitlesJob';
+
+import { KalturaStringValue } from './KalturaStringValue';
+import { KalturaRequest, KalturaRequestArgs } from '../kaltura-request';
+
+export interface AiMetadataGeneratorGenerateMetadataBySubtitlesActionArgs  extends KalturaRequestArgs {
+    subtitlesFileId : number;
+	externalAssetIds? : KalturaStringValue[];
+}
+
+/**
+ * Build request payload for service 'aiMetadataGenerator' action 'generateMetadataBySubtitles'.
+ *
+ * Usage: Start metadata generation process based on subtitles
+ *
+ * Server response type:         KalturaGenerateMetadataBySubtitlesJob
+ * Server failure response type: KalturaAPIException
+ */
+export class AiMetadataGeneratorGenerateMetadataBySubtitlesAction extends KalturaRequest<KalturaGenerateMetadataBySubtitlesJob> {
+
+    subtitlesFileId : number;
+	externalAssetIds : KalturaStringValue[];
+
+    constructor(data : AiMetadataGeneratorGenerateMetadataBySubtitlesActionArgs)
+    {
+        super(data, {responseType : 'o', responseSubType : 'KalturaGenerateMetadataBySubtitlesJob', responseConstructor : KalturaGenerateMetadataBySubtitlesJob  });
+        if (typeof this.externalAssetIds === 'undefined') this.externalAssetIds = [];
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                service : { type : 'c', default : 'aimetadatagenerator' },
+				action : { type : 'c', default : 'generateMetadataBySubtitles' },
+				subtitlesFileId : { type : 'n' },
+				externalAssetIds : { type : 'a', subTypeConstructor : KalturaStringValue, subType : 'KalturaStringValue' }
+            }
+        );
+        return result;
+    }
+}
+
