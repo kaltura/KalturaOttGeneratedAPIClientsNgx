a4695a76b8720d122bfa1196f41ac3320f385d3f
Backend CI <ott.rnd.core@kaltura.com>
2025-07-07 09:34:15 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1518, branch 11.4.0.2

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaBulkPlaybackContextResponse.ts
@@ -0,0 +1,38 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaBulkResponseItem } from './KalturaBulkResponseItem';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaBulkPlaybackContextResponseArgs  extends KalturaObjectBaseArgs {
+    items? : KalturaBulkResponseItem[];
+	totalCount? : number;
+}
+
+
+export class KalturaBulkPlaybackContextResponse extends KalturaObjectBase {
+
+    items : KalturaBulkResponseItem[];
+	totalCount : number;
+
+    constructor(data? : KalturaBulkPlaybackContextResponseArgs)
+    {
+        super(data);
+        if (typeof this.items === 'undefined') this.items = [];
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaBulkPlaybackContextResponse' },
+				items : { type : 'a', subTypeConstructor : KalturaBulkResponseItem, subType : 'KalturaBulkResponseItem' },
+				totalCount : { type : 'n' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaBulkPlaybackContextResponse'] = KalturaBulkPlaybackContextResponse;
