a8a93fdca6032d6a7ec23baca2b5b84826f01463
Backend CI <ott.rnd.core@kaltura.com>
2025-07-28 11:47:30 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1528, branch 11.5.0

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaBulkPlaybackContextRequest.ts
@@ -0,0 +1,35 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaGetPlaybackContextParams } from './KalturaGetPlaybackContextParams';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaBulkPlaybackContextRequestArgs  extends KalturaObjectBaseArgs {
+    playbackContextParamSets? : KalturaGetPlaybackContextParams[];
+}
+
+
+export class KalturaBulkPlaybackContextRequest extends KalturaObjectBase {
+
+    playbackContextParamSets : KalturaGetPlaybackContextParams[];
+
+    constructor(data? : KalturaBulkPlaybackContextRequestArgs)
+    {
+        super(data);
+        if (typeof this.playbackContextParamSets === 'undefined') this.playbackContextParamSets = [];
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaBulkPlaybackContextRequest' },
+				playbackContextParamSets : { type : 'a', subTypeConstructor : KalturaGetPlaybackContextParams, subType : 'KalturaGetPlaybackContextParams' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaBulkPlaybackContextRequest'] = KalturaBulkPlaybackContextRequest;
