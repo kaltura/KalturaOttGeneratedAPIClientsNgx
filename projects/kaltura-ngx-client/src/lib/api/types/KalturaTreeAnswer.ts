7a0e276893cebe4bb5a65dc426e5aa39daa12dee
Backend CI <ott.rnd.core@kaltura.com>
2025-08-06 09:21:33 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1531, branch 11.5.0.1

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaTreeAnswer.ts
@@ -0,0 +1,36 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaTreeAnswerArgs  extends KalturaObjectBaseArgs {
+    answerId? : string;
+	text? : string;
+}
+
+
+export class KalturaTreeAnswer extends KalturaObjectBase {
+
+    answerId : string;
+	text : string;
+
+    constructor(data? : KalturaTreeAnswerArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaTreeAnswer' },
+				answerId : { type : 's' },
+				text : { type : 's' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaTreeAnswer'] = KalturaTreeAnswer;
