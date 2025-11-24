62729a3631ca41837344660c6e26227b50706539
Backend CI <ott.rnd.core@kaltura.com>
2025-11-24 14:06:32 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1575, branch BEO-17769

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaSearchScope.ts
@@ -0,0 +1,39 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaSearchType } from './KalturaSearchType';
+import { KalturaSearchCondition } from './KalturaSearchCondition';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaSearchScopeArgs  extends KalturaObjectBaseArgs {
+    type? : KalturaSearchType;
+	filters? : KalturaSearchCondition[];
+}
+
+
+export class KalturaSearchScope extends KalturaObjectBase {
+
+    type : KalturaSearchType;
+	filters : KalturaSearchCondition[];
+
+    constructor(data? : KalturaSearchScopeArgs)
+    {
+        super(data);
+        if (typeof this.filters === 'undefined') this.filters = [];
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaSearchScope' },
+				type : { type : 'es', subTypeConstructor : KalturaSearchType, subType : 'KalturaSearchType' },
+				filters : { type : 'a', subTypeConstructor : KalturaSearchCondition, subType : 'KalturaSearchCondition' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaSearchScope'] = KalturaSearchScope;
