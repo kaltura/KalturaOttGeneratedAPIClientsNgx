68c02acd918fecb122803ad93106f04c3af9364a
Backend CI <ott.rnd.core@kaltura.com>
2026-02-18 09:41:15 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1602, branch fix_client_lib_generation

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaExternalAssetListResponse.ts
@@ -0,0 +1,38 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaAsset } from './KalturaAsset';
+import { KalturaAssetListResponse, KalturaAssetListResponseArgs } from './KalturaAssetListResponse';
+
+export interface KalturaExternalAssetListResponseArgs  extends KalturaAssetListResponseArgs {
+    externalQueryId? : string;
+	objects? : KalturaAsset[];
+}
+
+
+export class KalturaExternalAssetListResponse extends KalturaAssetListResponse {
+
+    externalQueryId : string;
+	objects : KalturaAsset[];
+
+    constructor(data? : KalturaExternalAssetListResponseArgs)
+    {
+        super(data);
+        if (typeof this.objects === 'undefined') this.objects = [];
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaExternalAssetListResponse' },
+				externalQueryId : { type : 's' },
+				objects : { type : 'a', subTypeConstructor : KalturaAsset, subType : 'KalturaAsset' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaExternalAssetListResponse'] = KalturaExternalAssetListResponse;
