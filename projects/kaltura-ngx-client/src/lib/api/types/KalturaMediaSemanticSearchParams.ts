44b43a435774a8098ac05a713668341b1c77e7be
Backend CI <ott.rnd.core@kaltura.com>
2026-03-03 14:17:48 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1603, branch 12.2.0.0

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaMediaSemanticSearchParams.ts
@@ -0,0 +1,32 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaMediaSemanticSearchParamsArgs  extends KalturaObjectBaseArgs {
+    
+}
+
+
+export class KalturaMediaSemanticSearchParams extends KalturaObjectBase {
+
+    
+
+    constructor(data? : KalturaMediaSemanticSearchParamsArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaMediaSemanticSearchParams' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaMediaSemanticSearchParams'] = KalturaMediaSemanticSearchParams;
