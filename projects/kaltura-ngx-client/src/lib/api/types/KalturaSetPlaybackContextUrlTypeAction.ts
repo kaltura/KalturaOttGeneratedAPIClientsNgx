53d15f7eaf91c3bc3597a8ead6c30579179c527b
Backend CI <ott.rnd.core@kaltura.com>
2026-03-05 14:46:20 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1604, branch 12.2.0.1

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaSetPlaybackContextUrlTypeAction.ts
@@ -0,0 +1,34 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaUrlType } from './KalturaUrlType';
+import { KalturaAssetRuleAction, KalturaAssetRuleActionArgs } from './KalturaAssetRuleAction';
+
+export interface KalturaSetPlaybackContextUrlTypeActionArgs  extends KalturaAssetRuleActionArgs {
+    urlType? : KalturaUrlType;
+}
+
+
+export class KalturaSetPlaybackContextUrlTypeAction extends KalturaAssetRuleAction {
+
+    urlType : KalturaUrlType;
+
+    constructor(data? : KalturaSetPlaybackContextUrlTypeActionArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaSetPlaybackContextUrlTypeAction' },
+				urlType : { type : 'es', subTypeConstructor : KalturaUrlType, subType : 'KalturaUrlType' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaSetPlaybackContextUrlTypeAction'] = KalturaSetPlaybackContextUrlTypeAction;
