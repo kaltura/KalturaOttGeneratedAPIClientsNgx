68869586c0ecb3c5adbc3d7ea4d05b6eddab7020
Backend CI <ott.rnd.core@kaltura.com>
2026-05-06 11:16:20 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1616, branch 1250-segmentation

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaTvodPurchasedCondition.ts
@@ -0,0 +1,39 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaBaseSegmentCondition, KalturaBaseSegmentConditionArgs } from './KalturaBaseSegmentCondition';
+
+export interface KalturaTvodPurchasedConditionArgs  extends KalturaBaseSegmentConditionArgs {
+    ppvIdEquals? : number;
+	mediaIdEquals? : number;
+	days? : number;
+}
+
+
+export class KalturaTvodPurchasedCondition extends KalturaBaseSegmentCondition {
+
+    ppvIdEquals : number;
+	mediaIdEquals : number;
+	days : number;
+
+    constructor(data? : KalturaTvodPurchasedConditionArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaTvodPurchasedCondition' },
+				ppvIdEquals : { type : 'n' },
+				mediaIdEquals : { type : 'n' },
+				days : { type : 'n' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaTvodPurchasedCondition'] = KalturaTvodPurchasedCondition;
---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaViewTimeConstraint.ts
@@ -0,0 +1,36 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaViewTimeConstraintArgs  extends KalturaObjectBaseArgs {
+    startTime? : string;
+	endTime? : string;
+}
+
+
+export class KalturaViewTimeConstraint extends KalturaObjectBase {
+
+    startTime : string;
+	endTime : string;
+
+    constructor(data? : KalturaViewTimeConstraintArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaViewTimeConstraint' },
+				startTime : { type : 's' },
+				endTime : { type : 's' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaViewTimeConstraint'] = KalturaViewTimeConstraint;
