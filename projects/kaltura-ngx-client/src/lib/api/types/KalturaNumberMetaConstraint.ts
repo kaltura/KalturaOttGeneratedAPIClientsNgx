24ef7eb017a67904b64d062f166b5100f6c8f9e0
Backend CI <ott.rnd.core@kaltura.com>
2026-08-27 13:04:24 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1641, branch 12.8.0.0

---
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaNumberMetaConstraint.ts
@@ -0,0 +1,39 @@
+
+import { KalturaObjectMetadata, typesMappingStorage } from '../kaltura-object-base';
+import { KalturaBaseAttributeConstraint, KalturaBaseAttributeConstraintArgs } from './KalturaBaseAttributeConstraint';
+
+export interface KalturaNumberMetaConstraintArgs  extends KalturaBaseAttributeConstraintArgs {
+    equals? : number;
+	greaterThan? : number;
+	smallerThan? : number;
+}
+
+
+export class KalturaNumberMetaConstraint extends KalturaBaseAttributeConstraint {
+
+    equals : number;
+	greaterThan : number;
+	smallerThan : number;
+
+    constructor(data? : KalturaNumberMetaConstraintArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaNumberMetaConstraint' },
+				equals : { type : 'n' },
+				greaterThan : { type : 'n' },
+				smallerThan : { type : 'n' }
+            }
+        );
+        return result;
+    }
+}
+
+typesMappingStorage['KalturaNumberMetaConstraint'] = KalturaNumberMetaConstraint;
